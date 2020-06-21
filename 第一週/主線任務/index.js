7a8d5e4244cfb06a9a4670817f1ab81a4b95b826
ShaoYu <shaoyu.kao@gmail.com>
2020-06-21 15:12:35 +0800

    完成第一週主線任務(LV2：僅使用課程版型，並重新撰寫 Todo List 功能)

--- 第一週/主線任務/index.js
+++ 第一週/主線任務/index.js
@@ -1,90 +1,122 @@
-var todoData = [];
+let todoData = [];
+const addTodo = document.getElementById('addTodo');
+const newTodo = document.getElementById('newTodo');
+const clearTask = document.getElementById('clearTask');
+const todoList = document.getElementById('todoList');
+const taskCount = document.getElementById('taskCount');
 
-document.getElementById('addTodo').addEventListener('click', function () {
-  var str = '';
-  if (document.getElementById('newTodo').value.trim() !== '') {
+/**
+ * 取得 TodoList 清單
+ *
+ * @returns TodoList 清單
+ */
+function getTodoListContent() {
+  let str = '';
+  todoData.forEach(function (item) {
+    str += `<li class="list-group-item">
+      <div class="d-flex">
+      <div class="form-check">
+      <input type="checkbox" class="form-check-input" ${item.completed ? 'checked' : ''} data-action="complete" data-id="${item.id}">
+      <label class="form-check-label ${item.completed ? 'completed' : ''}" data-action="complete" data-id="${item.id}"> ${item.title}</label>
+      </div>
+      <button type="button" class="close ml-auto" aria-label="Close">
+      <span aria-hidden="true" data-action="remove" data-id="${item.id}">&times;</span>
+      </button>
+      </div>
+      </li>`;
+  })
+
+  return str;
+}
+
+/**
+ * 新增 TodoList 資料
+ *
+ */
+function addTotoData() {
+  if (newTodo.value.trim() !== '') {
     todoData.push({
       id: Math.floor(Date.now()),
-      title: document.getElementById('newTodo').value,
+      title: newTodo.value,
       completed: false,
     })
-    todoData.forEach(function (item) {
-      str += `<li class="list-group-item">
-        <div class="d-flex">
-        <div class="form-check">
-        <input type="checkbox" class="form-check-input" ${item.completed ? 'checked' : ''} data-action="complete" data-id="${item.id}">
-        <label class="form-check-label ${item.completed ? 'completed' : ''}" data-action="complete" data-id="${item.id}"> ${item.title}</label>
-        </div>
-        <button type="button" class="close ml-auto" aria-label="Close">
-        <span aria-hidden="true" data-action="remove" data-id="${item.id}">&times;</span>
-        </button>
-        </div>
-        </li>`;
-    })
-    document.getElementById('todoList').innerHTML = str;
-    document.getElementById('taskCount').textContent = todoData.length;
-    document.getElementById('newTodo').value = '';
+    todoList.innerHTML = getTodoListContent();
+    taskCount.textContent = todoData.length;
+    newTodo.value = '';
   }
-});
-document.getElementById('clearTask').addEventListener('click', function (e) {
+}
+
+/**
+ * 清除所有 TodoList 任務資料
+ *
+ * @param {object} e Javascript MouseEvent
+ */
+function clearAllTotoData(e) {
+  console.log('clearAllTotoData', e);
   e.preventDefault();
   todoData = [];
-  document.getElementById('todoList').innerHTML = '';
-  document.getElementById('taskCount').textContent = todoData.length;
-});
-document.getElementById('todoList').addEventListener('click', function (e) {
-  var newIndex = 0;
-  if (e.target.dataset.action == 'remove') {
-    todoData.forEach(function (item, key) {
-      if (e.target.dataset.id == item.id) {
-        newIndex = key;
+  todoList.innerHTML = '';
+  taskCount.textContent = todoData.length;
+}
+
+/**
+ * 移除單一 TodoList 任務資料
+ *
+ * @param {string} removeId 所要移除資料 id
+ */
+function removeTotoData(removeId) {
+  let newIndex = 0;
+  todoData.forEach(function (item, key) {
+    if (removeId == item.id) {
+      newIndex = key;
+    }
+  })
+  todoData.splice(newIndex, 1);
+  todoList.innerHTML = getTodoListContent();
+  taskCount.textContent = todoData.length;
+}
+
+/**
+ * 切換使用者所點擊 TodoList 資料狀態
+ *
+ * @param {string} checkId 點擊 TodoList item 資料 id
+ */
+function checkCompleteClcikHandle(checkId) {
+  todoData.forEach(function (item) {
+    if (checkId == item.id) {
+      if (item.completed) {
+        item.completed = false;
+      } else {
+        item.completed = true;
       }
-    })
-    todoData.splice(newIndex, 1);
-    var str = '';
-    todoData.forEach(function (item) {
-      str += `<li class="list-group-item">
-        <div class="d-flex">
-        <div class="form-check">
-        <input type="checkbox" class="form-check-input" ${item.completed ? 'checked' : ''} data-action="complete" data-id="${item.id}">
-        <label class="form-check-label ${item.completed ? 'completed' : ''}" data-action="complete" data-id="${item.id}"> ${item.title}</label>
-        </div>
-        <button type="button" class="close ml-auto" aria-label="Close">
-        <span aria-hidden="true" data-action="remove" data-id="${item.id}">&times;</span>
-        </button>
-        </div>
-        </li>`;
-    });
-    document.getElementById('todoList').innerHTML = str;
-    document.getElementById('taskCount').textContent = todoData.length;
+    }
+  })
+  todoList.innerHTML = getTodoListContent();
+  taskCount.textContent = todoData.length;
+}
+
+/**
+ * 處理 TodoList Click Event
+ *
+ * @param {object} e Javascript MouseEvent
+ */
+function todoListClickHandle(e) {
+  let actionStatus = e.target.dataset.action;
+  if (actionStatus) {
+    switch (actionStatus) {
+      case 'remove':
+        removeTotoData(e.target.dataset.id);
+        break;
+      case 'complete':
+        checkCompleteClcikHandle(e.target.dataset.id);
+        break;
+      default:
+        break;
+    }
   }
-});
-document.getElementById('todoList').addEventListener('click', function (e) {
-  if (e.target.dataset.action == 'complete') {
-    todoData.forEach(function (item) {
-      if (e.target.dataset.id == item.id) {
-        if (item.completed) {
-          item.completed = false;
-        } else {
-          item.completed = true;
-        }
-      }
-    })
-    var str = '';
-    todoData.forEach(function (item) {
-      str += `<li class="list-group-item">
-        <div class="d-flex">
-        <div class="form-check">
-        <input type="checkbox" class="form-check-input" ${item.completed ? 'checked' : ''} data-action="complete" data-id="${item.id}">
-        <label class="form-check-label ${item.completed ? 'completed' : ''}" data-action="complete" data-id="${item.id}"> ${item.title}</label>
-        </div>
-        <button type="button" class="close ml-auto" aria-label="Close">
-        <span aria-hidden="true" data-action="remove" data-id="${item.id}">&times;</span>
-        </button>
-        </div>
-        </li>`;
-    })
-    document.getElementById('todoList').innerHTML = str;
-    document.getElementById('taskCount').textContent = todoData.length;
-  }
-});
+}
+
+addTodo.addEventListener('click', addTotoData);
+clearTask.addEventListener('click', clearAllTotoData);
+todoList.addEventListener('click', todoListClickHandle);
+
